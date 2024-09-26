37c7e2311708ab7b0c4c966d0bc19323939e24da
romain-gossiaux <romain.gossiaux@condorcet.be>
2024-09-26 14:10:03 +0200

    Commit initial

---
+++ lib/php/menu.php
@@ -0,0 +1,17 @@
+<ul class="nav justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link" href="index_.php?page=accueil">Accueil</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index_.php?page=page1">Page 1</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index_.php?page=page2">Page 2</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index_.php?page=page3">Page 3</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index_.php?page=page4">Page 4</a>
+    </li>
+</ul>
